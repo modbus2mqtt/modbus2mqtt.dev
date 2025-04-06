1038d79c27db6778219d2ff8559d6353cf6407be
Volkmar Nissen <info@carcam360.de>
2025-04-06 17:13:35 +0200

    Fix pull_request

--- bin/testall.py
+++ bin/testall.py
@@ -56,7 +56,7 @@ def startRequiredApps():
         # default directory
     with open("nginx.conf", "w") as g:
         g.write( nginxConf)
-    subprocess.Popen(["nohup", "nginx","-c","nginx.conf","-p","."])
+    subprocess.Popen(["nohup", "nginx","-c","./nginx.conf","-p","."])
     subprocess.Popen(["nohup", "node", "server/dist/runModbusTCPserver.js", "-y", "server/cypress/servers/modbustcp.conf/yaml-dir" , "--busid", "0"])
     for port in [3002,3006]:
         count=0
